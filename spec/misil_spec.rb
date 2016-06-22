1e6f1ab0f83d26013cc12bcdb57971a1d61e4a74
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-06-22 10:47:45 -0300

    Misil contiene un asteroide en un mapa en el momento de su creacion.

--- spec/misil_spec.rb
+++ spec/misil_spec.rb
@@ -3,16 +3,22 @@ require_relative '../model/misil'
 
 describe 'Misil' do
 
-  it 'should almacenar vida en su creacion' do
+  it 'should almacenar vida desde su creacion' do
     misil = Misil.new 100, 45
 
     expect(misil.vida).to eq 100
   end
 
-  it 'should almacenar masa en su creacion' do
+  it 'should almacenar masa desde su creacion' do
     misil = Misil.new 100, 45
 
     expect(misil.masa).to eq 45
+  end
+
+  it 'should contener un asteroide en un mapa desde su creacion' do
+    misil = Misil.new 100, 20
+
+    expect(misil.efectos.key?(Asteroide)).to eq true
   end
 
 end
