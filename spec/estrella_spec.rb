bdb84b2803cfc20dbbcf7dcfd1ae4849269c9c32
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-06-22 11:30:26 -0300

    Estrella contiene una bomba en un mapa en el momento de su creacion.

--- spec/estrella_spec.rb
+++ spec/estrella_spec.rb
@@ -3,13 +3,13 @@ require_relative '../model/estrella'
 
 describe 'Estrella' do
 
-  it 'should almacenar vida en su creacion' do
+  it 'should almacenar vida desde su creacion' do
     estrella = Estrella.new 70, 0
 
     expect(estrella.vida).to eq 70
   end
 
-  it 'should almacenar masa en su creacion' do
+  it 'should almacenar masa desde su creacion' do
     estrella = Estrella.new 100, 45
 
     expect(estrella.masa).to eq 45
@@ -19,6 +19,12 @@ describe 'Estrella' do
     estrella = Estrella.new 100, 45
 
     expect(estrella.efectos.key?(Asteroide)).to eq true
+  end
+
+  it 'should contener una bomba en un mapa desde su creacion' do
+    estrella = Estrella.new 100, 45
+
+    expect(estrella.efectos.key?(Bomba)).to eq true
   end
 
 end
