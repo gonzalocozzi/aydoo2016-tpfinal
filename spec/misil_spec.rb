90aae51967318c316efda3651fa7d3dd1831d058
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-06-22 10:01:28 -0300

    Misil almacena su masa en el momento de su creacion.

--- spec/misil_spec.rb
+++ spec/misil_spec.rb
@@ -4,9 +4,15 @@ require_relative '../model/misil'
 describe 'Misil' do
 
   it 'should almacenar vida en su creacion' do
-    misil = Misil.new 100
+    misil = Misil.new 100, 45
 
     expect(misil.vida).to eq 100
+  end
+
+  it 'should almacenar masa en su creacion' do
+    misil = Misil.new 100, 45
+
+    expect(misil.masa).to eq 45
   end
 
 end
