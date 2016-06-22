9996ae77dc2c79e7a6e2fc5903a6ffd0318f4cf9
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-06-22 10:36:01 -0300

    Nave contiene un asteroide en un mapa en el momento de su creacion.

--- spec/nave_spec.rb
+++ spec/nave_spec.rb
@@ -3,16 +3,22 @@ require_relative '../model/nave'
 
 describe 'Nave' do
 
-  it 'should almacenar vida en su creacion' do
+  it 'should almacenar vida desde su creacion' do
     nave = Nave.new 100, 0
 
     expect(nave.vida).to eq 100
   end
 
-  it 'should almacenar masa en su creacion' do
+  it 'should almacenar masa desde su creacion' do
     nave = Nave.new 100, 20
 
     expect(nave.masa).to eq 20
+  end
+
+  it 'should contener un asteroide en un mapa desde su creacion' do
+    nave = Nave.new 100, 20
+
+    expect(nave.efectos.key?(Asteroide)).to eq true
   end
 
 end
