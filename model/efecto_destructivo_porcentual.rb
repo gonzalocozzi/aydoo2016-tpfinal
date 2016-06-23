db762e1afea52cf5b60c1dccae40ee4d9a832e96
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-06-23 11:33:58 -0300

    Efecto destructivo porcentual quita vida a un objeto espacial.

--- model/efecto_destructivo_porcentual.rb
+++ model/efecto_destructivo_porcentual.rb
@@ -6,4 +6,9 @@ class EfectoDestructivoPorcentual
     @variacion_de_vida = variacion_de_vida
   end
 
+  def aplicar_efecto(objeto_colisionador)
+    variacion_de_vida_del_objeto_colisionador = objeto_colisionador.vida * @variacion_de_vida
+    objeto_colisionador.vida -= variacion_de_vida_del_objeto_colisionador
+  end
+
 end
--- spec/efecto_destructivo_porcentual_spec.rb
+++ spec/efecto_destructivo_porcentual_spec.rb
@@ -9,4 +9,13 @@ describe 'EfectoDestructivoPorcentual' do
     expect(efecto_destructivo_porcentual.variacion_de_vida).to eq 0.3
   end
 
+  it 'should quitar vida a un objeto espacial' do
+    efecto_destructivo_porcentual = EfectoDestructivoPorcentual.new 0.5
+    estrella = Estrella.new 80, 100
+
+    efecto_destructivo_porcentual.aplicar_efecto estrella
+
+    expect(estrella.vida).to eq 40
+  end
+
 end
