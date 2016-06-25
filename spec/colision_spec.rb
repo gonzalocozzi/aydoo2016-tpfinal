99f79b6129aae8d1cd82fe1ca8332e6c0c7caa30
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-06-25 16:28:30 -0300

    Colisión debería quitarle 100 puntos de vida a una nave que es colisionada por otra nave.

--- spec/colision_spec.rb
+++ spec/colision_spec.rb
@@ -14,4 +14,14 @@ describe 'Colision' do
     expect(nave_1.vida).to eq 10
   end
 
+  it 'deberia quitarle 100 puntos de vida a una nave que es colisionada por otra nave' do
+    nave_1 = Nave.new 110, 20
+    nave_2 = Nave.new 250, 15
+    colision = Colision.new
+
+    colision.colisionar nave_1, nave_2
+
+    expect(nave_2.vida).to eq 150
+  end
+
 end
