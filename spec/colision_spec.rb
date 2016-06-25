1d52ee468be2c6bb0b4eaa84c38e66a40f5566f3
Gonzalo Alejandro Cozzi <gonzalocozzi@outlook.com>
2016-06-25 17:07:12 -0300

    Colisión debería no provocar cambios en la masa de un misil que colisiona con un asteroide.

--- spec/colision_spec.rb
+++ spec/colision_spec.rb
@@ -168,4 +168,15 @@ describe 'Colision' do
     expect(misil.vida).to eq 115
   end
 
+  it 'deberia no provocar cambios en la masa de un misil que colisiona con un asteroide' do
+    misil = Misil.new 115, 90
+    asteroide = Asteroide.new
+    colision = Colision.new
+
+    colision.colisionar misil, asteroide
+
+    expect(misil.masa).to eq 90
+  end
+
+
 end
